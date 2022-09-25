bee321d981320e2560c8651ddf3f7746bc622963
binarstrike <binarnugroho775@gmail.com>
2022-09-25 03:01:17 +0000

    update: variabel dan tipe data

---
+++ Variabel dan Tipe data/index.js
@@ -0,0 +1,10 @@
+// deklarasi variabel
+var nama = "Binar Nugroho";
+var age = 17;
+var title  = "Belajar Javascript";
+
+// menampilkan data dari variabel ke console
+console.log("Nama Saya " + nama + " umur Saya " + age + "\n" + title);
+
+// variabel dengan tipe data boolean
+var isBoy = true;
