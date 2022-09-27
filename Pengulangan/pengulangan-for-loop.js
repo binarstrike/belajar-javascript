50d2a5ff425171bc634ee2a96149e4192b360c9b
binarstrike <binarnugroho775@gmail.com>
2022-09-27 02:53:55 +0000

    update: Pengulangan

---
+++ Pengulangan/pengulangan-for-loop.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+var x = 10;
+
+
+for (var y = 1; y < x; y++) {
+    console.log("Halo Ucup " + y);
+}
---
+++ Pengulangan/pengulangan-while-loop.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+var x = 10;
+var y = 1;
+
+while (x > y) {
+    console.log("Halo Ucup " + x);
+    x--;
+}
